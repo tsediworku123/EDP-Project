c51fac62623f69b789ab2b6dcd2a71e5f5a9b9bf
tsediworku123 <tsedeniaworku457@gmail.com>
2026-03-08 22:54:42 +0300

    Add project files.

---
+++ Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicPatientSystem.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+    }
+}
