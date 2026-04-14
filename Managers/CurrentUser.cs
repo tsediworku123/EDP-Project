490ea0c21c175dced03828875ae6663e6ce51aaa
Haielemeskel-Getnaeh <hailegetaneh1221@gmail.com>
2026-04-15 01:00:04 +0300

    chore: re-index project and apply complete gitignore patterns

--- Managers/CurrentUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ClinicAppointmentSystem
-{
-    public static class CurrentUser
-    {
-        public static int Id { get; set; }
-        public static string Role { get; set; } = "";
-        public static string Username { get; set; } = "";
-    }
-}
